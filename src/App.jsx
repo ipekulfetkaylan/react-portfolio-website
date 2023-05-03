43e5d016a8ff89caf7c061b4365d04d9e14d7b6a
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-05-03 15:17:22 +0300

    topbar added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./app.scss";
+import Intro from "./components/intro/Intro";
+import Topbar from "./components/topbar/Topbar";
+import Portfolio from"./components/portfolio/Portfolio";
+import Testimonials from"./components/testimonials/Testimonials";
+import Works from"./components/work/Works";
+import Contact from"./components/contact/Contact";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
+
+
+function App() {
+  const [menuOpen, setMenuOpen]=useState(true);
+  
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Testimonials/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import "./topbar.scss";
+import PersonIcon from '@mui/icons-material/Person';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+
+
+export default function Topbar({menuOpen,setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+       <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo"><b>F</b><small>D.</small> </a>
+          <div className="temContainer">
+           <PersonIcon className="icon"/>
+           <span >+90 535 4554</span>
+          </div>
+          <div className="temContainer">
+          <MailOutlineIcon className="icon"/> 
+           <span>ipek@fd.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>{setMenuOpen(!menuOpen)}}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+       </div>
+      
+    </div>
+  )
+}
