e66542ea22d8c81385c194ac9c88fb6dfd4e1774
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-05-04 15:10:46 +0300

    portfolio added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
-import "./intro.scss"
+import "./intro.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+       showCursor: false,
+       backDelay: 1500,
+       strings: ["Developer", "Content Creator"]
+      });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/woman1.png" alt="woman" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm </h2>
+          <h1>İpek Kaylan</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>{" "}
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownIcon className="icon" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "webApp",
+      title: "Web App",
+    },
+    {
+      id: "mobileApp",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "branding",
+      title: "Branding",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => {
+          return (
+            <PortfolioList
+              title={item.title}
+              id={item.id}
+              active={selected === item.id}
+              setSelected={setSelected}
+            />
+          );
+        })}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/bankApp.webp" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/bankApp.webp" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/bankApp.webp" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
